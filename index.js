251d39eb32aef729c99bbef6a079ca94967267e8
abdallah608 <abdallahhassanshaaban@gmail.com>
2023-05-29 21:46:48 +0300

    first commit

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import { connection } from './dataBase/connection/connection.js'
 import { bookRouter } from './src/bookModule/book.router.js'
 import { userRouter } from './src/userModule/user.router.js';
 import globalError from './utilities/error/globalError.js'
+import appError from './utilities/error/appError.js';
 // server express
 const app = express()
 const port = process.env.port || 8000
